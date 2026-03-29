816525527370b67241a09af0b204a77510d873fb
TOOLS-PANEL <xdestructff@gmail.com>
2026-03-29 18:23:51 +0700

    Update get-data.js

--- api/get-data.js
+++ api/get-data.js
@@ -1,9 +1,13 @@
-// api/get-data.js - Ambil data untuk website
+// api/get-data.js - Dengan no-cache agar real-time
 
 import { getAllData } from './_db.js';
 
 export default async function handler(req, res) {
+    // Header untuk mencegah cache
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
     
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
